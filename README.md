1ef4a5be5476dccf38d113572707e1322b4b155d
unknown <joseph@odd-e.com>
2024-02-02 17:57:17 +0800

    :wrench: update cmake and readme to get a successful build

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ It uses CMake to generate a build and Vcpkg for dependency management.
 Using the template and opening the folder in IDEs like VSCode should be enough, there the recommended plugins should be installed and the correct preset selected.
 
 Currently it will install 2 dependencies, Catch2 for unit tests and fmt for text formatting and printing.
+
+# How to build
+```
+mkdir build
+cd build
+cmake -A Win32 -S ..
+cmake --build .
+```
+Please note that `-A Win32` is quite important here since the detours lib installed via vcpkg only supports 32 bit builds.
--- src/FindDirectX.cmake
+++ src/FindDirectX.cmake
@@ -6,17 +6,22 @@
 #  DirectX_FOUND, If false, do not try to use DirectX.
 #  DirectX_ROOT_DIR, directory where DirectX was installed.
 
-set(DirectX_INCLUDE_DIRS "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include")
+#set(DirectX_INCLUDE_DIRS "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include")
+find_path(CMAKE_INCLUDE_ROOT "directxsdk/d3dx9.h")
+set(DirectX_INCLUDE_DIRS "${CMAKE_INCLUDE_ROOT}/directxsdk")
 
-get_filename_component(DirectX_ROOT_DIR "${DirectX_INCLUDE_DIRS}/.." ABSOLUTE)
+#get_filename_component(DirectX_ROOT_DIR "${DirectX_INCLUDE_DIRS}/.." ABSOLUTE)
 
-set(DirectX_LIBRARY_PATHS "${DirectX_ROOT_DIR}/Lib/x86" "${DirectX_ROOT_DIR}/Lib")
+#set(DirectX_LIBRARY_PATHS "${DirectX_ROOT_DIR}/Lib/x86" "${DirectX_ROOT_DIR}/Lib")
 
-find_library(DirectX_D3D9_LIBRARY d3d9 ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
-find_library(DirectX_D3DX9_LIBRARY d3dx9 ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
+#find_library(DirectX_D3D9_LIBRARY d3d9 ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
+find_library(DirectX_D3D9_LIBRARY d3d9 "C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.22621.0\\um\\x86" NO_DEFAULT_PATH)
+#find_library(DirectX_D3DX9_LIBRARY d3dx9 ${DirectX_LIBRARY_PATHS} NO_DEFAULT_PATH)
+find_library(DirectX_D3DX9_LIBRARY d3dx9 REQUIRED)
 set(DirectX_LIBRARIES ${DirectX_D3D9_LIBRARY} ${DirectX_D3DX9_LIBRARY})
 
 # handle the QUIETLY and REQUIRED arguments and set DirectX_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(DirectX DEFAULT_MSG DirectX_ROOT_DIR DirectX_LIBRARIES DirectX_INCLUDE_DIRS)
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(DirectX DEFAULT_MSG DirectX_ROOT_DIR DirectX_LIBRARIES DirectX_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(DirectX DEFAULT_MSG DirectX_LIBRARIES DirectX_INCLUDE_DIRS)
 mark_as_advanced(DirectX_INCLUDE_DIRS DirectX_D3D9_LIBRARY DirectX_D3DX9_LIBRARY)
